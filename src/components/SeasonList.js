e24aec34f4cdf7eb605f1489d999bdf327f3be12
Tissa Khosla <tissa.music@gmail.com>
2019-11-17 17:49:25 -0500

    season buttons are links

--- src/components/SeasonList.js
+++ src/components/SeasonList.js
@@ -2,14 +2,13 @@ import React from 'react'
 import SeasonItem from './SeasonItem'
 
 const SeasonList = (props) => {
-
-  return (
-    <div>
-      <p>{props.orchestras}</p>
-      <p>{props.seasons}</p>
-    </div>
-  )
-
+  let seasonArray = props.seasons.map(season => {
+    let url = `http://localhost:8080/program/season/${season}`
+    return (
+    <button><a href={url}>{season}</a></button>
+    )  
+  })
+  return <div>{seasonArray}</div>
 }
 
 export default SeasonList;
